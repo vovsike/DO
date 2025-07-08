1d7a9e4fe6bee2b83afe6f733eca7247b35f36a6
vovsike <vladimir.garanin@stfc.ac.uk>
2025-07-08 22:47:59 +0100

    Add terraform for managing basic engines, policy

---
+++ vault/terraform/main.tf
@@ -0,0 +1,43 @@
+resource "vault_mount" "fia_kv2" {
+  path = "fia"
+  type = "kv-v2"
+  options = {version = "2"}
+}
+
+resource "vault_mount" "fase_kv2" {
+  path = "fase"
+  type = "kv-v2"
+  options = {version = "2"}
+}
+
+resource "vault_mount" "isis_observability_kv2" {
+  path = "isis_observability"
+  type = "kv-v2"
+  options = {version = "2"}
+}
+
+resource "vault_mount" "isis_security_kv2" {
+  path = "isis_security"
+  type = "kv-v2"
+  options = {version = "2"}
+}
+
+resource "vault_mount" "fia_db" {
+  path = "fia_db"
+  type = "database"
+}
+
+resource "vault_mount" "fase_db" {
+  path = "fase_db"
+  type = "database"
+}
+
+resource "vault_mount" "fia_pki" {
+  path = "fia_pki"
+  type = "pki"
+}
+
+resource "vault_mount" "isis_observability_pki" {
+  path = "isis_observability_pki"
+  type = "pki"
+}
--- vault/terraform/policies.tf
+++ vault/terraform/policies.tf
@@ -1,4 +1,20 @@
-resource "vault_policy" "team1_dev_policy" {
-    name = "team1_dev"
-    policy = file("../policies/team1.hcl")
+resource "vault_policy" "isis_admin" {
+    name = "isis_admin"
+    policy = file("../policies/isis_admin.hcl")
+}
+
+resource "vault_policy" "super_admin" {
+    name = "super_admin"
+    policy = file("../policies/super_admin.hcl")
+}
+
+resource "vault_policy" "fia_admin" {
+  name = "fia_admin"
+  policy = file("../policies/fia_admin.hcl")
+}
+
+
+resource "vault_policy" "fase_admin" {
+  name = "fase_admin"
+  policy = file("../policies/fase_admin.hcl")
 }
